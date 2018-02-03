e0b7f531eb52b4b03b094c3c1143970b05bc7b85
Manuel Ruck <github@manuelruck.de>
2018-02-03 11:04:48 +0100

    try to sort

--- src/graphql/resolvers/Procedure.js
+++ src/graphql/resolvers/Procedure.js
@@ -1,8 +1,26 @@
 export default {
   Query: {
-    getProcedures: (parent, { offset = 0, pageSize = 20 }, { ProcedureModel }) =>
-      ProcedureModel.find()
-        .skip(offset)
-        .limit(pageSize),
+    getProcedures: (parent, { offset = 0, pageSize = 20 }, { ProcedureModel }) => {
+      console.log();
+      const date = new Date();
+      date.setDate(date.getDate() + 7);
+      return ProcedureModel.aggregate([
+        { $match: { 'history.initiator': '2. Beratung' } },
+        {
+          $addFields: {
+            order: {
+              $filter: {
+                input: '$history',
+                as: 'p',
+                cond: { $eq: ['$$p.initiator', '2. Beratung'] },
+              },
+            },
+          },
+        },
+        { $sort: { order: -1 } },
+        { $limit: pageSize + offset },
+        { $skip: offset },
+      ]).then(res => console.log(res));
+    },
   },
 };
