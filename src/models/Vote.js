4f72929393f1be732a0fad9b03b087adef401023
Manuel Ruck <github@manuelruck.de>
2018-03-21 17:36:52 +0100

    add vote model

---
+++ src/models/Vote.js
@@ -0,0 +1,14 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+import mongoose, { Schema } from 'mongoose';
+
+const VoteSchema = new Schema({
+  procedure: { type: Schema.Types.ObjectId, ref: 'Procedure', required: true },
+  users: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+  voteResults: {
+    yes: { type: Number, default: 0 },
+    no: { type: Number, default: 0 },
+    abstination: { type: Number, default: 0 },
+  },
+});
+
+export default mongoose.model('Vote', VoteSchema);
