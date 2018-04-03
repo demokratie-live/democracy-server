030251b92d2257fd213e4bccdc242f314ac17981
Manuel Ruck <github@manuelruck.de>
2018-04-03 17:18:56 +0200

    fix import all on wrong procedure number

--- src/graphql/queries/getProcedureUpdates.js
+++ src/graphql/queries/getProcedureUpdates.js
@@ -4,7 +4,7 @@ export default gql`
   query procedureUpdates($period: [Int!], $type: [String!]) {
     procedureUpdates(period: $period, type: $type) {
       procedureId
-      updatedAt
+      bioUpdateAt
     }
   }
 `;
--- src/graphql/queries/getProcedures.js
+++ src/graphql/queries/getProcedures.js
@@ -11,6 +11,7 @@ export default gql`
       abstract
       tags
       subjectGroups
+      bioUpdateAt
       history {
         assignment
         initiator
