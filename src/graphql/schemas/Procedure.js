801994f0c176109ed62938940e6354dddcc4dd76
Manuel Ruck <github@manuelruck.de>
2018-06-16 09:24:08 +0200

    add old searchProcedurs functionality add find procedure by procedureId or docId

--- src/graphql/schemas/Procedure.js
+++ src/graphql/schemas/Procedure.js
@@ -37,6 +37,7 @@ type Query {
   procedure(id: ID!): Procedure
   procedures(type: ProcedureType!, pageSize: Int, offset: Int): [Procedure]
   notifiedProcedures: [Procedure]
-  searchProcedures(term: String!): SearchProcedures
+  searchProcedures(term: String!): [Procedure] @deprecated(reason: "use searchProceduresAutocomplete")
+  searchProceduresAutocomplete(term: String!): SearchProcedures
 }
 `;
