2e5cb57c93d3f39ca4537fec2ba9314dc6697538
Manuel Ruck <github@manuelruck.de>
2018-08-09 14:50:48 +0200

    fix paths #2

--- src/migrations/scripts/create.js
+++ src/migrations/scripts/create.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 const config = JSON.parse(fs.readFileSync('./mm.json'));
 const migrator = new mm.Migrator(config);
 console.log(process.argv[2]);
-migrator.create(`${__dirname}/src/migrations/`, process.argv[2], (err) => {
+migrator.create(`${__dirname}`, process.argv[2], (err) => {
   if (err) {
     console.log(err);
   }
