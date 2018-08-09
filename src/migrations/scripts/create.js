c48d9d35ebdd34da39ab2984a3b6b53be7a6673d
Manuel Ruck <github@manuelruck.de>
2018-08-09 14:49:18 +0200

    fix paths

--- src/migrations/scripts/create.js
+++ src/migrations/scripts/create.js
@@ -1,10 +1,10 @@
 import mm from 'mongodb-migrations';
 import fs from 'fs';
 
-const config = JSON.parse(fs.readFileSync('../../../mm.json'));
+const config = JSON.parse(fs.readFileSync('./mm.json'));
 const migrator = new mm.Migrator(config);
 console.log(process.argv[2]);
-migrator.create('../', process.argv[2], (err) => {
+migrator.create(`${__dirname}/src/migrations/`, process.argv[2], (err) => {
   if (err) {
     console.log(err);
   }
