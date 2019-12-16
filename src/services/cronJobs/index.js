ca77de1b9dca24df09c4fc3767a88fc219565711
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2019-12-16 19:56:34 +0100

    fixed cron order

--- src/services/cronJobs/index.js
+++ src/services/cronJobs/index.js
@@ -21,8 +21,8 @@ const cronJobs = () => [
   // new CronJob('55 3 * * *', resetCronSuccessStartDate, null, true, 'Europe/Berlin'),
 
   // PUSHS
-  new CronJob('0 14 * * SUN', quePushsConferenceWeek, null, true, 'Europe/Berlin'),
   new CronJob('*/15 7-22 * * *', sendQuedPushs, null, true, 'Europe/Berlin'),
+  new CronJob('0 14 * * SUN', quePushsConferenceWeek, null, true, 'Europe/Berlin'),
   new CronJob('0 4 * * MON-FRI', quePushsVoteTop100, null, true, 'Europe/Berlin'),
   new CronJob('0 4 * * MON-FRI', quePushsVoteConferenceWeek, null, true, 'Europe/Berlin'),
   // new CronJob('0 8 * * *', sendNotifications, null, true, 'Europe/Berlin'),
