dc42e84d15c6754689d7ecdbc2cd81e1ee90d144
Manuel Ruck <github.com@manuelruck.de>
2020-02-05 22:00:07 +0100

    fix eslint errors

--- src/data/conference-weeks.js
+++ src/data/conference-weeks.js
@@ -12,17 +12,17 @@ function parseDate(input) {
 
 const getWeekNumber = d => {
   // Copy date so don't modify original
-  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  const date = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
   // Set to nearest Thursday: current date + 4 - current day number
   // Make Sunday's day number 7
-  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
   // Get first day of year
-  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  const yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
   // Calculate full weeks to nearest Thursday
-  const weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+  const weekNo = Math.ceil(((date - yearStart) / 86400000 + 1) / 7);
   // Return array of year and week number
-//   return [d.getUTCFullYear(), weekNo];
-   return weekNo;
+  //   return [date.getUTCFullYear(), weekNo];
+  return weekNo;
 };
 
 export const conferenceWeeks = [
@@ -130,10 +130,9 @@ export const getCurrentConferenceWeek = () => {
   }
 
   // else return next conference week
-  const nextConferenceWeek = conferenceWeeks.find(({ start, end }) => {
+  const nextConferenceWeek = conferenceWeeks.find(({ start }) => {
     return curDate < start;
   });
 
-  console.log(getWeekNumber(nextConferenceWeek.start))
   return { ...nextConferenceWeek, calendarWeek: getWeekNumber(nextConferenceWeek.start) };
 };
--- src/graphql/resolvers/ConferenceWeek.js
+++ src/graphql/resolvers/ConferenceWeek.js
@@ -1,11 +1,8 @@
-/* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
-import { Types } from 'mongoose';
-import CONFIG from '../../config';
 import { getCurrentConferenceWeek } from '../../data/conference-weeks';
 
 export default {
   Query: {
-    currentConferenceWeek: async (parent, _, { ProcedureModel, ActivityModel, user }) => {
+    currentConferenceWeek: async () => {
       Log.graphql('ConferenceWeek.query.currentConferenceWeek');
       return getCurrentConferenceWeek();
     },
